11c0f3a166e3dfd97e2bb62c8401eee8ee1080e8
binaryThinker <114472374+ibarraaa@users.noreply.github.com>
2025-08-27 17:20:58 +0800

    Update and rename for-loop to for-loop.java

---
+++ for-loop.java
@@ -0,0 +1,44 @@
+
+// package java fundamentals;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+       
+       
+       Scanner s = new Scanner(System.in);
+       
+       
+       
+       String[] names = {"David", "Ace", "allen", "jasfer",  "jaymar"};
+       System.out.print("Name To Search: ");
+       String search = s.nextLine();
+       
+       
+       for(String name:names){
+           System.out.println(name);
+           break;
+       }
+       System.out.println("_condition in for-each loop;__");
+       // condition in for-each loop;
+       
+       for(String name:names){
+           if(name.equalsIgnoreCase("Jasfer")){
+               System.out.println("WE FOUND: " + name);
+               break;
+           }else{
+               System.out.println(name);
+           }
+       }
+       System.out.println("_________________________"); 
+        System.out.println("array of integers SUMMATION");
+       // array of integers SUMMATION;
+       
+       int numbers[] = {1123, 234, 3843, 4634, 5334, 555, 3346, 7636, 8363, 936};
+       int sum = 0;
+       for(int number:numbers){
+           sum += number;
+       }
+       System.out.println("Sum: " + sum);
+       
+    }
+}
